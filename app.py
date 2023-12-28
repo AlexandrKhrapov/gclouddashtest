b02021c6a2994be244e6e0569c9ad24cf6f7e05e
AlexandrKhrapov <155019016+AlexandrKhrapov@users.noreply.github.com>
2023-12-28 13:56:23 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+# Source of this code: https://dash.plotly.com/minimal-app
+
+# One minor modification was made in order to make this file compatible with the 
+# Cloud Run setup. See the Readme for more details.
+
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash(__name__)
+
+server = app.server # Added in for Cloud Run compatibility
+
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
+
+initialized_variable = 5
+
+if initialized_variable == 5:
+    print("Hello")
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+if __name__ == '__main__':
+    app.run(debug=True)
